ff1d4e977e19d9d26ba13192f8f89aafb705037a
Leonardo Martinez <leonardo.martinez@conna.sv>
2020-11-23 16:09:14 -0600

    Added database backup

---
+++ JobsBoard.Server/JobsBoard.Server.BL/Jobs/JobsBL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobsBoard.Server.DL;
+using JobsBoard.Server.EL;
+
+namespace JobsBoard.Server.BL
+{
+    public class JobsBL
+    {
+        JobBoardEntities cx = new JobBoardEntities();
+
+        public ReplyEL JobsListBL(Data01EL Data)
+        {
+            ReplyEL Re = new ReplyEL();
+            JobsList_Result val = cx.JobsList(Pagination);
+            if (val.RES == "OK")
+            {
+                Re.Reply = cx.JobsList(Data.Pagination);
+                Re.UsToken = Data.UsToken;
+                Re.UsCuentaID = Data.UsCuentaID;
+            }
+            else
+            {
+                Res.UsMsj = val.RES;
+                Res.UsCuentaID = -1;
+            }
+
+            return Res;
+            return Re;
+        }
+    }
+}
---
+++ JobsBoard.Server/JobsBoard.Server.EL/ReplyEL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsBoard.Server.EL
+{
+    public class ReplyEL
+    {
+        public Object Reply { get; set; }
+
+        public Int32 UsCuentaID { get; set; }
+        public String UsToken { get; set; }
+        public String UsMsj { get; set; }
+
+        public ReplyEL()
+        {
+            this.Reply = new Object();
+            this.UsCuentaID = -1;
+            this.UsToken = "";
+            this.UsMsj = "";
+        }
+    }
+
+    public class Data01EL
+    {
+        public Int32 Pagination { get; set; }
+        public Int32 UsCuentaID { get; set; }
+        public String UsToken { get; set; }
+
+
+        public ReplyEL()
+        {
+            this.Pagination = -1;
+            this.UsCuentaID = -1;
+            this.UsToken = "";
+        }
+    }
+}
